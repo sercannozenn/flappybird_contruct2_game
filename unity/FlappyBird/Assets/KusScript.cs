51a9d21a0d8a4a2d5d4e05aedb5ebc3e8378c794
Sercan <srcn.ozn5@gmail.com>
2019-08-22 09:39:52 +0300

    First Upload

---
+++ unity/FlappyBird/Assets/KusScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KusScript : MonoBehaviour
+{
+    public float hiz;
+    public float ziplamaGucu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * hiz);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * ziplamaGucu * Time.deltaTime);
+        }
+    }
+}
